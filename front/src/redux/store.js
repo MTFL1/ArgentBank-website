cba6ac5ab0002b6abd4a5d2bbec54b397ac23c37
Mohamed tefili <m.ecomplus1@gmail.com>
2023-10-25 21:49:59 +0200

    add persiStore redux

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,10 +1,22 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+ import storage from 'redux-persist/lib/storage';
+
+ const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 
-const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ const persistConfig = {
+    key: 'root', // Clé de persistance
+    storage,
+ };
 
-const store = createStore(rootReducer, reduxDevtools);
 
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer); //
+
+const store = createStore(persistedReducer, reduxDevtools);
+ const persistor = persistStore(store);
+
+
+ export { store, persistor }; 
